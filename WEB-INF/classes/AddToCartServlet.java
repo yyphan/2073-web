72bf517e56790007f34b6f7659d455ab2e676cf5
Yao Yifan <jeromeyao1999@gmail.com>
2022-02-28 15:38:53 +0800

    Cart function is up

---
+++ WEB-INF/classes/AddToCartServlet.java
@@ -0,0 +1,34 @@
+import java.io.*;
+import java.sql.*;
+import jakarta.servlet.*;            // Tomcat 10
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+@WebServlet("/addtocart") 
+public class AddToCartServlet extends HttpServlet {
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response)
+               throws ServletException, IOException {
+        PrintWriter out=response.getWriter();
+        String product_id = request.getParameter("product_id");
+          
+        HttpSession session=request.getSession(false);  
+        if(session!=null){ 
+            // data is like [1001, 2]: 2 of product with id 1001 were added in cart
+            if (session.getAttribute(product_id) != null)
+            {
+                // increment count if the product was already added
+                int count = ((Integer) session.getAttribute(product_id)).intValue();
+                session.setAttribute(product_id, count + 1);
+            } else {
+                // add the product if not added in cart
+                session.setAttribute(product_id, 1);
+            }
+        }  
+        else{  
+            out.print("Please login first");  
+            request.getRequestDispatcher("login.html").include(request, response);  
+        }  
+        out.close();  
+    }
+}
--- WEB-INF/classes/CartServlet.java
+++ WEB-INF/classes/CartServlet.java
@@ -13,14 +13,47 @@ public class CartServlet extends HttpServlet {
         request.getRequestDispatcher("link.html").include(request, response);  
           
         HttpSession session=request.getSession(false);  
-        if(session!=null){  
-        String name=(String)session.getAttribute("name");  
-          
-        out.print("Hello, "+name+" Welcome to Cart");  
-        }  
-        else{  
+        if(session==null){  
             out.print("Please login first");  
-            request.getRequestDispatcher("login.html").include(request, response);  
+            request.getRequestDispatcher("login.html").include(request, response); 
+        }  
+        else{   
+            String name=(String)session.getAttribute("name");  
+            out.print("Hello, "+name+" Welcome to Your Cart");  
+
+            try (
+                Connection conn = DriverManager.getConnection(
+                    "jdbc:mysql://localhost:3306/newecommerce?allowPublicKeyRetrieval=true&useSSL=false&serverTimezone=UTC",
+                    "myuser", "xxxx"
+                ); 
+                
+                Statement stmt = conn.createStatement();
+            ) {
+                String sqlStr = "SELECT * FROM products;";
+                ResultSet rset = stmt.executeQuery(sqlStr);
+                // loop through all products to see if any of them was added in cart
+                while (rset.next()) {
+                    int id = rset.getInt("id");
+                    String idStr = Integer.toString(id);
+
+                    // if the product with that id was added in cart
+                    if (session.getAttribute(idStr) != null)
+                    {
+                        String description = rset.getString("description");
+                        float singlePrice = rset.getFloat("price");
+                        int count = ((Integer)session.getAttribute(idStr)).intValue();
+                        float totalPrice = singlePrice * count;
+                        // display the item
+                        out.println("<p>" + description + "</p>");
+                        out.println("<p>" + count + "</p>");
+                        out.println("<p>" + totalPrice + "</p>");
+                    }
+                }
+            } catch (Exception ex) {
+                out.println("<p>Error: " + ex.getMessage() + "</p>");
+                out.println("<p>Check Tomcat console for details.</p>");
+                ex.printStackTrace();
+            }
         }  
         out.close();  
     }  
